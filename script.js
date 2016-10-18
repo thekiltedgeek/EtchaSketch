d63bbaf14e2c05c1bfe3b6032dba5eed0134d63a
Larry Shanahan <thekiltedgeek@gmail.com>
2016-10-18 18:36:43 -0400

    Added code so user can select number of cells per side and fit into current space.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
      var div = $('#grid');
+     var cel = $('.cel');
+     var divSize = div.width();
+     var mrgSize = 2;
      $('input[name=resetButton]').click(function(){
 	  var num = prompt('Enter number of squares per side');
 	  div.empty();
+	  var newSize = divSize/num - mrgSize*2;
 	  for(var i=0; i< num*num; i++){
 	       div.append('<div class="cel"></div>');
 	  }
+	  $('.cel').width(newSize);
+	  $('.cel').height(newSize);
      });
 });
