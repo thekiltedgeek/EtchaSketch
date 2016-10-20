6f039e3b27b11b6447c016cff34912704f93cb1c
Larry Shanahan <thekiltedgeek@gmail.com>
2016-10-19 20:20:33 -0400

    Put grid drawing routine into own jquery function for re-usability.  Also made minor adjstments to css file.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 $(document).ready(function(){
-     var div = $('#grid');
-     var divSize = div.width();
-     var mrgSize = 2;
-     
+     $(document).drawGrid(10);
      $('input[name=resetButton]').click(function(){
-	  var num = prompt('Enter number of squares per side');
-	  div.empty();
-	  var newSize = divSize/num - mrgSize*2;
-	  for(var i=0; i< num*num; i++){
-	       div.append('<div class="cel"></div>');
-	  }
-	  $('.cel').width(newSize);
-	  $('.cel').height(newSize);
-	  
-	  $('.cel').mouseenter(function(){
-	       $(this).addClass('visited');
-	  });
+         var num = prompt('Enter number of squares per side');
+         $(document).drawGrid(num);
      });
-     
-     
 });
+
+/* Re-usable function to draw the grid with sides = num */
+$.fn.drawGrid=function(num){
+    var div = $('#grid');
+    var divSize = div.width();
+    var mrgSize = 2;
+    
+    div.empty();
+    var newSize = divSize/num - mrgSize * 2;
+    for(var i=0; i<num*num; i++){
+        div.append('<div class="cel"></div>');
+    }
+    $('.cel').width(newSize);
+    $('.cel').height(newSize);
+    
+    $('.cel').mouseenter(function(){
+        $(this).addClass('visited');
+    });
+}
